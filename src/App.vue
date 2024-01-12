f3a625c10eab95a3c27efa43e3eff6175518cc30
DarrenViljoen32 <31759385+DarrenViljoen32@users.noreply.github.com>
2024-01-12 12:55:27 +0200

    first push

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- calling form -->
+<SignUp/>
+</template>
+
+<script>
+// importing form
+import SignUp from './components/SignUp.vue'
+export default {
+  name: 'App',
+  components: {
+    SignUp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin: 0px;
+  background: #696969;
+  
+}
+
+*{
+  color: rgb(84, 161, 228);
+}
+</style>
